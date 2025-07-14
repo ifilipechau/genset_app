36d925bff08c3f750b93ec986f158ed0e20d3faf
ifilipechau <filipechau.dev@gmail.com>
2025-07-14 16:05:23 +0200

    fix: remove streamlit features

--- genset.py
+++ genset.py
@@ -2,11 +2,7 @@
 
 
 # Importa bibliotecas necessárias
-import streamlit as st
 import datetime
-
-# Define o título da aba e o layout da página
-st.set_page_config(page_title="Dimensionador de Gerador", layout="centered")
 
 def inserir_equipamento():
     equipamentos = []
